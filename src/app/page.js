d8d097757cacb9a8465b13ce0bd9c1387deedf95
halkaui <148041628+halkaui@users.noreply.github.com>
2023-10-16 17:33:39 +0600

    project just uploaded

---
+++ src/app/page.js
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-between p-24">
+      <p className="text-white text-3xl font-semibold">Welcome to Halka UI</p>
+    </div>
+  );
+}

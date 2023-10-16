612cf3da7fb117f07651b7cee92f8e170c9b88f6
halkaui <148041628+halkaui@users.noreply.github.com>
2023-10-16 17:37:15 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "placehold.jp",
+      port: "",
+      pathname: "",
+    },
+  ],
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
